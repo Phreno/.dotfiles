1f9cddba78be4cdb98d36d06a14858b7002120dd
phreno <losgoomy@gmail.com>
2022-08-13 17:22:54 +0200

    déplace de la conf (todo reécrire init.vim en lua

---
+++ conf/nvim.lua
@@ -0,0 +1,11 @@
+vim.opt.listchars = {
+    eol      = '⤶',
+    space    = '❤',
+    trail    = '✚',
+    extends  = '◀',
+    precedes = '▶',
+    tab      = '⇥',
+    tabstop  = '⇥',
+    nbsp     = '␣',
+    space    = '·'
+}

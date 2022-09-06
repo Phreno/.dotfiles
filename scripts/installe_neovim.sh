d14dd4334a92acb427ed592813f057e0c73cb91e
phreno <losgoomy@gmail.com>
2022-09-06 13:06:54 +0200

    encore un test pour nvim

--- scripts/installe_neovim.sh
+++ scripts/installe_neovim.sh
@@ -2,8 +2,8 @@
 installe_neovim(){
   #wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage -O ~/bin/nvim
   wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -O /tmp/nvim
-  tar -xvf /tmp/nvim -C ~/bin/.nvim
-  ln -s ~/bin/.nvim/nvim-linux64/bin/nvim ~/bin/nvim
+  tar -xvf /tmp/nvim -C ~/bin/
+  ln -s ~/bin/nvim-linux64/bin/nvim ~/bin/nvim
 }
 
 main(){

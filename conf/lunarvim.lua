523460159dd323e0f7a8033a86585da2647205c4
Etienne Audouin <etienne.audouin-ext@pole-emploi.fr>
2022-09-08 11:55:32 +0200

    ajoute de la config vim

--- conf/lunarvim.lua
+++ conf/lunarvim.lua
@@ -12,7 +12,9 @@ lvim.transparent_window = true
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
+-- lvim.colorscheme = "onedarker"
+lvim.colorscheme = "kanagawa"
+
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -206,7 +208,6 @@ lvim.plugins = {
     event = "BufRead",
     config = function() require "lsp_signature".on_attach() end,
   },
-  { "wellle/context.vim" },
   { "zbirenbaum/copilot.lua",
     event = { "VimEnter" },
     panel = {
@@ -313,7 +314,36 @@ lvim.plugins = {
       vim.api.nvim_command("augroup END")
     end
   },
-  { "godlygeek/tabular" }
+  { "godlygeek/tabular" },
+  { "vimwiki/vimwiki" },
+  { "gpanders/editorconfig.nvim" },
+  {
+    "romgrk/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup {
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        throttle = true, -- Throttles plugin updates (may improve performance)
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+          -- For all filetypes
+          -- Note that setting an entry here replaces all other patterns for this entry.
+          -- By setting the 'default' entry below, you can control which nodes you want to
+          -- appear in the context window.
+          default = {
+            'class',
+            'function',
+            'method',
+          },
+        },
+      }
+    end
+  },
+  { "tpope/vim-speeddating" },
+  { "rebelot/kanagawa.nvim" },
+  {
+    "rktjmp/lush.nvim",
+  },
+
 }
 -- Can not be placed into the config method of the plugins.
 lvim.builtin.cmp.formatting.source_names["copilot"] = "(Copilot)"

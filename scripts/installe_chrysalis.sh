4d95bd1d62709c17abf71fa79ff95f99de8fa366
phreno <losgoomy@gmail.com>
2022-09-11 19:25:19 +0200

    ajoute un peu de config et update chrysalis

--- scripts/installe_chrysalis.sh
+++ scripts/installe_chrysalis.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 installe_chrysalis(){
-  wget  https://github.com/keyboardio/Chrysalis/releases/download/v0.11.0/Chrysalis-0.11.0.AppImage -O ~/bin/chrysalis
-    chmod +x ~/bin/chrysalis
+  # wget  https://github.com/keyboardio/Chrysalis/releases/download/v0.11.0/Chrysalis-0.11.0.AppImage -O ~/bin/chrysalis
+  wget https://github.com/keyboardio/Chrysalis/releases/download/v0.11.1/Chrysalis-0.11.1.AppImage -O ~/bin/chrysalis
+  chmod +x ~/bin/chrysalis
 }
 main(){
 installe_chrysalis

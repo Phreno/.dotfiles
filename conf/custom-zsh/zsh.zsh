30de6f1e98f623c9243a4a4bd8be152ed823e875
phreno <losgoomy@gmail.com>
2022-09-13 22:06:38 +0200

    Corrige les noms de fichiers et fait un peu de nettoyage

---
+++ conf/custom-zsh/zsh.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# Développeur ....: K3rn€l_P4n1K
+# Nom ............: zsh
+# Description ....: Configuration personnelle de zsh
+# Version ........: 1.0
+# Date ...........: Fri Jul 29 21:31:48 CEST 2022
+# Dépendances ....: zsh
+#
+
+alias vim='lvim'
+alias v='lvim'
+
+
+
+PATH="$HOME/bin/nvim-linux64/bin/nvim:$HOME/bin:$HOME/.local/bin:$PATH"
+

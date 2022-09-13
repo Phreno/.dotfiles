d0c082c5082e9661100ce8c7a703889cd7bb5e25
Etienne Audouin <etienne.audouin-ext@pole-emploi.fr>
2022-09-13 15:16:42 +0200

    Configure Tyrannical

--- conf/awesome/rc.lua
+++ conf/awesome/rc.lua
@@ -175,17 +175,17 @@ awful.screen.connect_for_each_screen(function(s)
   set_wallpaper(s)
 
   -- Each screen has its own tag table.
-  awful.tag({
-    "1. term",
-    "2. dev",
-    "3. board",
-    "4. web",
-    "5. social",
-    "6. fun",
-    "7. conf",
-    "8. training",
-    "9. administration"
-  }, s, awful.layout.layouts[1])
+  -- awful.tag({
+  --   "1. term",
+  --   "2. dev",
+  --   "3. board",
+  --   "4. web",
+  --   "5. social",
+  --   "6. fun",
+  --   "7. conf",
+  --   "8. training",
+  --   "9. administration"
+  -- }, s, awful.layout.layouts[1])
 
   -- Create a promptbox for each screen
   s.mypromptbox = awful.widget.prompt()
@@ -648,8 +648,120 @@ client.connect_signal("manage", function(c)
   c.shape = gears.shape.rounded_rect
 end)
 
+
+----------------
+-- TYRANNICAL --
+----------------
+-- {{{
+local tyrannical = require("tyrannical")
+tyrannical.tags = {
+  -----------------
+  -- Main screen --
+  -----------------
+  {
+    name      = "Term", -- Call the tag "Term"
+    init      = true, -- Load the tag on startup
+    exclusive = true, -- Refuse any other type of clients (by classes)
+    screen    = 2, -- Create this tag on screen 1 and screen 2
+    layout    = awful.layout.suit.tile, -- Use the tile layout
+    instance  = { "dev", "ops" }, -- Accept the following instances. This takes precedence over 'class'
+    class     = { --Accept the following classes, refuse everything else (because of "exclusive=true")
+      "alacritty"
+    }
+  },
+  {
+    name      = "Develop",
+    init      = true,
+    exclusive = true,
+    screen    = 2,
+    layout    = awful.layout.suit.max,
+    class     = {
+      "Code"
+    }
+  },
+  ----------------------
+  -- Secondary screen --
+  ----------------------
+  {
+    name      = "Internet",
+    init      = true,
+    exclusive = true,
+    --icon        = "~net.png",                 -- Use this icon for the tag (uncomment with a real path)
+    screen    = 1, -- Setup on screen 2 if there is more than 1 screen, else on screen 1
+    layout    = awful.layout.suit.max, -- Use the max layout
+    class     = {
+      "Firefox", "brave"
+    }
+  },
+  {
+    name      = "Social",
+    init      = true,
+    exclusive = true,
+    --icon        = "~net.png",                 -- Use this icon for the tag (uncomment with a real path)
+    screen    = 1, -- Setup on screen 2 if there is more than 1 screen, else on screen 1
+    layout    = awful.layout.suit.max, -- Use the max layout
+    class     = {
+      "Teams"
+    }
+  },
+  {
+    name      = "Files",
+    init      = true,
+    exclusive = true,
+    screen    = 1,
+    layout    = awful.layout.suit.tile,
+    exec_once = { "dolphin" }, --When the tag is accessed for the first time, execute this command
+    class     = {
+      "Thunar", "Konqueror", "Dolphin", "ark", "Nautilus", "emelfm"
+    }
+  },
+  {
+    name      = "Doc",
+    init      = false, -- This tag wont be created at startup, but will be when one of the
+    screen    = 1,
+    -- client in the "class" section will start. It will be created on
+    -- the client startup screen
+    exclusive = true,
+    layout    = awful.layout.suit.max,
+    class     = {
+      "Assistant", "Okular", "Evince", "EPDFviewer", "xpdf",
+      "Xpdf",
+    }
+  },
+}
+
+-- Ignore the tag "exclusive" property for the following clients (matched by classes)
+tyrannical.properties.intrusive = {
+  "ksnapshot", "pinentry", "gtksu", "kcalc", "xcalc",
+  "feh", "Gradient editor", "About KDE", "Paste Special", "Background color",
+  "kcolorchooser", "plasmoidviewer", "Xephyr", "kruler", "plasmaengineexplorer",
+}
+
+-- Ignore the tiled layout for the matching clients
+tyrannical.properties.floating = {
+  "MPlayer", "pinentry", "ksnapshot", "pinentry", "gtksu",
+  "xine", "feh", "kmix", "kcalc", "xcalc",
+  "yakuake", "Select Color$", "kruler", "kcolorchooser", "Paste Special",
+  "New Form", "Insert Picture", "kcharselect", "mythfrontend", "plasmoidviewer"
+}
+
+-- Make the matching clients (by classes) on top of the default layout
+tyrannical.properties.ontop = {
+  "Xephyr", "ksnapshot", "kruler"
+}
+
+-- Force the matching clients (by classes) to be centered on the screen on init
+tyrannical.properties.placement = {
+  kcalc = awful.placement.centered
+}
+
+tyrannical.settings.block_children_focus_stealing = true --Block popups ()
+tyrannical.settings.group_children = true --Force popups/dialogs to have the same tags as the parent client
+
+-- }}}
+
+
 -- Autostart
 beautiful.useless_gap = 1
 awful.spawn.with_shell("picom -f -c")
 awful.spawn.with_shell("nitrogen --restore")
-awful.spawn.with_shell("alacritty")

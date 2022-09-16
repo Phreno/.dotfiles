c45f3f1743aab97723a6ee9155cd78434900b22b
phreno <losgoomy@gmail.com>
2022-09-16 22:50:52 +0200

    refacto(wiki): Ajoute des aliases simplifiés pour le wiki

--- conf/zsh/wiki.zsh
+++ conf/zsh/wiki.zsh
@@ -8,3 +8,9 @@ alias dotfiles.wiki_restart='wiki.stop; wiki.start'
 alias dotfiles.wiki_files='find -L ~/neuron -type f -name "*md"'
 alias dotfiles.wiki_watch='find -L ~/neuron -type f -name "*md" | entr -s "systemctl --user stop neuron.service;systemctl --user start neuron.service"'
 
+alias dw?='dotfiles.wiki_status'
+alias dwk='dotfiles.wiki_stop'
+alias dws='dotfiles.wiki_start'
+alias dwr='dotfiles.wiki_restart'
+alias dww='dotfiles.wiki_watch'
+

62c904986331e0a2f09ff9de9f2e5c6167784c14
phreno <losgoomy@gmail.com>
2022-09-16 22:33:22 +0200

    refacto(wiki): prefixe les aliases par dotfiles

--- conf/zsh/wiki.zsh
+++ conf/zsh/wiki.zsh
@@ -1,10 +1,10 @@
 #!/bin/bash
-alias wiki.reload_daemon='systemctl --user daemon-reload'
-alias wiki.status='systemctl --user status neuron.service'
-alias wiki.enable='systemctl --user enable neuron.service'
-alias wiki.stop='systemctl --user stop neuron.service'
-alias wiki.start='systemctl --user start neuron.service'
-alias wiki.restart='wiki.stop; wiki.start'
-alias wiki.files='find -L ~/neuron -type f -name "*md"'
-alias wiki.watch='find -L ~/neuron -type f -name "*md" | entr -s "systemctl --user stop neuron.service;systemctl --user start neuron.service"'
+alias dotfiles.wiki_reload_daemon='systemctl --user daemon-reload'
+alias dotfiles.wiki_status='systemctl --user status neuron.service'
+alias dotfiles.wiki_enable='systemctl --user enable neuron.service'
+alias dotfiles.wiki_stop='systemctl --user stop neuron.service'
+alias dotfiles.wiki_start='systemctl --user start neuron.service'
+alias dotfiles.wiki_restart='wiki.stop; wiki.start'
+alias dotfiles.wiki_files='find -L ~/neuron -type f -name "*md"'
+alias dotfiles.wiki_watch='find -L ~/neuron -type f -name "*md" | entr -s "systemctl --user stop neuron.service;systemctl --user start neuron.service"'
 

0b8a15c17020f71e9336ceb2d1b987ad17b90068
phreno <losgoomy@gmail.com>
2022-09-19 21:49:53 +0200

    dev(search): Ajoute une image json des notes pour la recherche

---
+++ neuron/notes.js
@@ -0,0 +1,709 @@
+let notes = [
+  {
+    "Date": "2022-09-19T14:07",
+    "ID": "3f3c6861",
+    "Meta": {
+      "date": "2022-09-19T14:07",
+      "tags": [
+        "developpement/java/spring",
+        "root/developpement/java/spring"
+      ]
+    },
+    "Path": "./developpement/java/spring/3f3c6861.md",
+    "Slug": "3f3c6861",
+    "Title": "Spring"
+  },
+  {
+    "Date": "2022-09-19T15:32",
+    "ID": "40a8fc13",
+    "Meta": {
+      "date": "2022-09-19T15:32",
+      "tags": [
+        "root/vim",
+        "vim"
+      ]
+    },
+    "Path": "./vim/40a8fc13.md",
+    "Slug": "40a8fc13",
+    "Title": "Mastering vim visual mode"
+  },
+  {
+    "ID": "765cb78f",
+    "Meta": {
+      "tags": [
+        "root/developpement/java/spring/spring-boot"
+      ]
+    },
+    "Path": "./developpement/java/spring/spring-boot/765cb78f.md",
+    "Slug": "765cb78f",
+    "Title": "FAQ Spring Boot"
+  },
+  {
+    "Date": "2022-09-19T13:52",
+    "ID": "76ce7c43",
+    "Meta": {
+      "date": "2022-09-19T13:52",
+      "tags": [
+        "interview/decouplage",
+        "root/interview/decouplage"
+      ]
+    },
+    "Path": "./interview/decouplage/76ce7c43.md",
+    "Slug": "76ce7c43",
+    "Title": "Découplage format"
+  },
+  {
+    "Date": "2022-09-19T13:54",
+    "ID": "7c087f79",
+    "Meta": {
+      "date": "2022-09-19T13:54",
+      "tags": [
+        "interview/decouplage",
+        "root/interview/decouplage"
+      ]
+    },
+    "Path": "./interview/decouplage/7c087f79.md",
+    "Slug": "7c087f79",
+    "Title": "Découplage temporalité"
+  },
+  {
+    "Date": "2022-09-19T14:04",
+    "ID": "84f24d00",
+    "Meta": {
+      "date": "2022-09-19T14:04",
+      "tags": [
+        "developpement/java/spring/spring-boot",
+        "root/developpement/java/spring/spring-boot"
+      ]
+    },
+    "Path": "./developpement/java/spring/spring-boot/84f24d00.md",
+    "Slug": "84f24d00",
+    "Title": "Spring Boot"
+  },
+  {
+    "Date": "2022-09-18T20:42",
+    "ID": "8f0a0c6e",
+    "Meta": {
+      "date": "2022-09-18T20:42",
+      "tags": [
+        "configuration/awsome/nitrogen",
+        "crontab",
+        "root/configuration/awsome/nitrogen"
+      ]
+    },
+    "Path": "./configuration/awsome/nitrogen/8f0a0c6e.md",
+    "Slug": "8f0a0c6e",
+    "Title": "Définir un fond d’écran dynamique avec Nitrogen et crontab"
+  },
+  {
+    "Date": "2022-09-18T14:41",
+    "ID": "90d3d5cf",
+    "Meta": {
+      "date": "2022-09-18T14:41",
+      "tags": [
+        "bdd/nosql",
+        "bdd/sql",
+        "interview",
+        "root/interview"
+      ]
+    },
+    "Path": "./interview/90d3d5cf.md",
+    "Slug": "90d3d5cf",
+    "Title": "Différence entre SQL et noSql"
+  },
+  {
+    "Date": "2022-09-19T13:53",
+    "ID": "92a2be13",
+    "Meta": {
+      "date": "2022-09-19T13:53",
+      "tags": [
+        "interview/decouplage",
+        "root/interview/decouplage"
+      ]
+    },
+    "Path": "./interview/decouplage/92a2be13.md",
+    "Slug": "92a2be13",
+    "Title": "Découplage localisation"
+  },
+  {
+    "Date": "2022-09-18T15:46",
+    "ID": "9a331780",
+    "Meta": {
+      "date": "2022-09-18T15:46",
+      "tags": [
+        "neovim/snippets",
+        "root/neovim/snippets"
+      ]
+    },
+    "Path": "./neovim/snippets/9a331780.md",
+    "Slug": "9a331780",
+    "Title": "Snippets dans LunarVim"
+  },
+  {
+    "Date": "2022-09-19T12:01",
+    "ID": "a433cf48",
+    "Meta": {
+      "date": "2022-09-19T12:01",
+      "tags": [
+        "neovim/snippets/bash-snippets",
+        "root/neovim/snippets/bash-snippets"
+      ]
+    },
+    "Path": "./neovim/snippets/bash-snippets/a433cf48.md",
+    "Slug": "a433cf48",
+    "Title": "bash check empty"
+  },
+  {
+    "Date": "2022-09-19T13:51",
+    "ID": "aadb5c25",
+    "Meta": {
+      "date": "2022-09-19T13:51",
+      "tags": [
+        "interview/decouplage",
+        "root/interview/decouplage"
+      ]
+    },
+    "Path": "./interview/decouplage/aadb5c25.md",
+    "Slug": "aadb5c25",
+    "Title": "Couplage Decouplage"
+  },
+  {
+    "ID": "awesome",
+    "Meta": {
+      "tags": [
+        "root/configuration"
+      ]
+    },
+    "Path": "./configuration/awesome",
+    "Slug": "awesome",
+    "Title": "awesome/"
+  },
+  {
+    "ID": "awsome",
+    "Meta": {
+      "tags": [
+        "root/configuration"
+      ]
+    },
+    "Path": "./configuration/awsome",
+    "Slug": "awsome",
+    "Title": "awsome/"
+  },
+  {
+    "Date": "2022-09-18T11:07",
+    "ID": "b4b836bf",
+    "Meta": {
+      "date": "2022-09-18T11:07",
+      "tags": [
+        "root/test/secret",
+        "test/secret"
+      ]
+    },
+    "Path": "./test/secret/b4b836bf.md",
+    "Slug": "b4b836bf",
+    "Title": "Test"
+  },
+  {
+    "ID": "b9bf79d2",
+    "Meta": {
+      "tags": [
+        "root/memoire/nombre/major-system"
+      ]
+    },
+    "Path": "./memoire/nombre/major-system/b9bf79d2.md",
+    "Slug": "b9bf79d2",
+    "Title": "Major system"
+  },
+  {
+    "ID": "bash",
+    "Meta": {
+      "tags": [
+        "root/developpement"
+      ]
+    },
+    "Path": "./developpement/bash",
+    "Slug": "bash",
+    "Title": "bash/"
+  },
+  {
+    "ID": "bash-snippets",
+    "Meta": {
+      "tags": [
+        "root/neovim/snippets"
+      ]
+    },
+    "Path": "./neovim/snippets/bash-snippets",
+    "Slug": "bash-snippets",
+    "Title": "bash-snippets/"
+  },
+  {
+    "ID": "bug",
+    "Meta": {
+      "tags": [
+        "root/configuration/wiki"
+      ]
+    },
+    "Path": "./configuration/wiki/bug",
+    "Slug": "bug",
+    "Title": "bug/"
+  },
+  {
+    "Date": "2022-09-19T14:10",
+    "ID": "c11a3848",
+    "Meta": {
+      "date": "2022-09-19T14:10",
+      "tags": [
+        "root/todo",
+        "todo"
+      ]
+    },
+    "Path": "./todo/c11a3848.md",
+    "Slug": "c11a3848",
+    "Title": "TODO"
+  },
+  {
+    "ID": "c8965e26",
+    "Meta": {
+      "date": "dim. 18 sept. 2022 15:23:44 CEST",
+      "tags": [
+        "developpement/java/spring/spring-boot",
+        "interview",
+        "root/developpement/java/spring/spring-boot"
+      ]
+    },
+    "Path": "./developpement/java/spring/spring-boot/c8965e26.md",
+    "Slug": "c8965e26",
+    "Title": "Différences entre Spring et Spring Boot"
+  },
+  {
+    "Date": "2022-09-19T17:50",
+    "ID": "cb25c292",
+    "Meta": {
+      "date": "2022-09-19T17:50",
+      "tags": [
+        "developpement/web/standalone",
+        "root/developpement/web/standalone"
+      ]
+    },
+    "Path": "./developpement/web/standalone/cb25c292.md",
+    "Slug": "cb25c292",
+    "Title": "MiniSearch: Client Side search engine"
+  },
+  {
+    "ID": "configuration",
+    "Meta": {
+      "tags": [
+        "root"
+      ]
+    },
+    "Path": "./configuration",
+    "Slug": "configuration",
+    "Title": "configuration/"
+  },
+  {
+    "ID": "configuration-awsome",
+    "Meta": {
+      "tags": [
+        "root/configuration/awsome"
+      ]
+    },
+    "Path": "./configuration/awsome/configuration-awsome.md",
+    "Slug": "configuration-awsome",
+    "Title": "configuration-awsome"
+  },
+  {
+    "ID": "configuration-index",
+    "Meta": {
+      "tags": [
+        "root/configuration"
+      ]
+    },
+    "Path": "./configuration/configuration-index.md",
+    "Slug": "configuration-index",
+    "Title": "configuration-index"
+  },
+  {
+    "ID": "configuration-lazygit",
+    "Meta": {
+      "tags": [
+        "root/configuration/lazy"
+      ]
+    },
+    "Path": "./configuration/lazy/configuration-lazygit.md",
+    "Slug": "configuration-lazygit",
+    "Title": "configuration-lazygit"
+  },
+  {
+    "ID": "configuration-neuron",
+    "Meta": {
+      "tags": [
+        "root/configuration/wiki"
+      ]
+    },
+    "Path": "./configuration/wiki/configuration-neuron.md",
+    "Slug": "configuration-neuron",
+    "Title": "configuration-neuron"
+  },
+  {
+    "ID": "configuration-revelation",
+    "Meta": {
+      "tags": [
+        "root/configuration/awsome/revelation"
+      ]
+    },
+    "Path": "./configuration/awsome/revelation/configuration-revelation.md",
+    "Slug": "configuration-revelation",
+    "Title": "configuration-revelation"
+  },
+  {
+    "ID": "configuration-service-wiki",
+    "Meta": {
+      "tags": [
+        "root/configuration/wiki"
+      ]
+    },
+    "Path": "./configuration/wiki/configuration-service-wiki.md",
+    "Slug": "configuration-service-wiki",
+    "Title": "Mise en place du service Neuron"
+  },
+  {
+    "ID": "decouplage",
+    "Meta": {
+      "tags": [
+        "root/interview"
+      ]
+    },
+    "Path": "./interview/decouplage",
+    "Slug": "decouplage",
+    "Title": "decouplage/"
+  },
+  {
+    "ID": "developpement",
+    "Meta": {
+      "tags": [
+        "root"
+      ]
+    },
+    "Path": "./developpement",
+    "Slug": "developpement",
+    "Title": "developpement/"
+  },
+  {
+    "Date": "2022-09-19T14:08",
+    "ID": "eb68f631",
+    "Meta": {
+      "date": "2022-09-19T14:08",
+      "tags": [
+        "developpement/git",
+        "root/developpement/git"
+      ]
+    },
+    "Path": "./developpement/git/eb68f631.md",
+    "Slug": "eb68f631",
+    "Title": "Git: Ignorer une partie d’un fichier"
+  },
+  {
+    "Date": "2022-09-19T08:05",
+    "ID": "f0ea1a34",
+    "Meta": {
+      "date": "2022-09-19T08:05",
+      "tags": [
+        "configuration/wiki/bug",
+        "root/configuration/wiki/bug"
+      ]
+    },
+    "Path": "./configuration/wiki/bug/f0ea1a34.md",
+    "Slug": "f0ea1a34",
+    "Title": "neuron open Problème de droits dans le navigateur"
+  },
+  {
+    "Date": "2022-09-19T12:34",
+    "ID": "fe21f02f",
+    "Meta": {
+      "date": "2022-09-19T12:34",
+      "tags": [
+        "developpement/bash",
+        "root/developpement/bash"
+      ]
+    },
+    "Path": "./developpement/bash/fe21f02f.md",
+    "Slug": "fe21f02f",
+    "Title": "Supprimer les dossiers vides"
+  },
+  {
+    "ID": "git",
+    "Meta": {
+      "tags": [
+        "root/developpement"
+      ]
+    },
+    "Path": "./developpement/git",
+    "Slug": "git",
+    "Title": "git/"
+  },
+  {
+    "ID": "index",
+    "Meta": {
+      "tags": []
+    },
+    "Path": "./index.md",
+    "Slug": "index",
+    "Title": "Index notes"
+  },
+  {
+    "ID": "installation-neuron",
+    "Meta": {
+      "tags": [
+        "root/configuration/wiki"
+      ]
+    },
+    "Path": "./configuration/wiki/installation-neuron.md",
+    "Slug": "installation-neuron",
+    "Title": "installation-neuron"
+  },
+  {
+    "ID": "interview",
+    "Meta": {
+      "tags": [
+        "root"
+      ]
+    },
+    "Path": "./interview",
+    "Slug": "interview",
+    "Title": "interview/"
+  },
+  {
+    "ID": "java",
+    "Meta": {
+      "tags": [
+        "root/developpement"
+      ]
+    },
+    "Path": "./developpement/java",
+    "Slug": "java",
+    "Title": "java/"
+  },
+  {
+    "ID": "lazy",
+    "Meta": {
+      "tags": [
+        "root/configuration"
+      ]
+    },
+    "Path": "./configuration/lazy",
+    "Slug": "lazy",
+    "Title": "lazy/"
+  },
+  {
+    "ID": "major-system",
+    "Meta": {
+      "tags": [
+        "root/memoire/nombre"
+      ]
+    },
+    "Path": "./memoire/nombre/major-system",
+    "Slug": "major-system",
+    "Title": "major-system/"
+  },
+  {
+    "ID": "memoire",
+    "Meta": {
+      "tags": [
+        "root"
+      ]
+    },
+    "Path": "./memoire",
+    "Slug": "memoire",
+    "Title": "memoire/"
+  },
+  {
+    "ID": "neovim",
+    "Meta": {
+      "tags": [
+        "root"
+      ]
+    },
+    "Path": "./neovim",
+    "Slug": "neovim",
+    "Title": "neovim/"
+  },
+  {
+    "ID": "nitrogen",
+    "Meta": {
+      "tags": [
+        "root/configuration/awsome"
+      ]
+    },
+    "Path": "./configuration/awsome/nitrogen",
+    "Slug": "nitrogen",
+    "Title": "nitrogen/"
+  },
+  {
+    "ID": "nombre",
+    "Meta": {
+      "tags": [
+        "root/memoire"
+      ]
+    },
+    "Path": "./memoire/nombre",
+    "Slug": "nombre",
+    "Title": "nombre/"
+  },
+  {
+    "ID": "nvim",
+    "Meta": {
+      "tags": [
+        "root/configuration"
+      ]
+    },
+    "Path": "./configuration/nvim",
+    "Slug": "nvim",
+    "Title": "nvim/"
+  },
+  {
+    "ID": "nvim-tree",
+    "Meta": {
+      "tags": [
+        "root/configuration/nvim"
+      ]
+    },
+    "Path": "./configuration/nvim/nvim-tree.md",
+    "Slug": "nvim-tree",
+    "Title": "nvim-tree"
+  },
+  {
+    "ID": "revelation",
+    "Meta": {
+      "tags": [
+        "root/configuration/awsome"
+      ]
+    },
+    "Path": "./configuration/awsome/revelation",
+    "Slug": "revelation",
+    "Title": "revelation/"
+  },
+  {
+    "ID": "secret-index",
+    "Meta": {
+      "tags": [
+        "root/secret"
+      ]
+    },
+    "Path": "./secret/secret-index.md",
+    "Slug": "secret-index",
+    "Title": "secret-index"
+  },
+  {
+    "ID": "snippets",
+    "Meta": {
+      "tags": [
+        "root/neovim"
+      ]
+    },
+    "Path": "./neovim/snippets",
+    "Slug": "snippets",
+    "Title": "snippets/"
+  },
+  {
+    "ID": "spring",
+    "Meta": {
+      "tags": [
+        "root/developpement/java"
+      ]
+    },
+    "Path": "./developpement/java/spring",
+    "Slug": "spring",
+    "Title": "spring/"
+  },
+  {
+    "ID": "spring-boot",
+    "Meta": {
+      "tags": [
+        "root/developpement/java/spring"
+      ]
+    },
+    "Path": "./developpement/java/spring/spring-boot",
+    "Slug": "spring-boot",
+    "Title": "spring-boot/"
+  },
+  {
+    "ID": "standalone",
+    "Meta": {
+      "tags": [
+        "root/developpement/web"
+      ]
+    },
+    "Path": "./developpement/web/standalone",
+    "Slug": "standalone",
+    "Title": "standalone/"
+  },
+  {
+    "ID": "test",
+    "Meta": {
+      "tags": [
+        "root"
+      ]
+    },
+    "Path": "./test",
+    "Slug": "test",
+    "Title": "test/"
+  },
+  {
+    "ID": "todo",
+    "Meta": {
+      "tags": [
+        "root"
+      ]
+    },
+    "Path": "./todo",
+    "Slug": "todo",
+    "Title": "todo/"
+  },
+  {
+    "ID": "tyrannical",
+    "Meta": {
+      "date": "dim. 18 sept. 2022 21:28:52 CEST",
+      "tags": [
+        "configuration/awesome/tyrannical",
+        "root/configuration/awesome",
+        "root/configuration/awesome/tyrannical"
+      ]
+    },
+    "Path": "./configuration/awesome/tyrannical/tyrannical.md",
+    "Slug": "tyrannical",
+    "Title": "Configuration de Tyrannical"
+  },
+  {
+    "ID": "vim",
+    "Meta": {
+      "tags": [
+        "root"
+      ]
+    },
+    "Path": "./vim",
+    "Slug": "vim",
+    "Title": "vim/"
+  },
+  {
+    "ID": "web",
+    "Meta": {
+      "tags": [
+        "root/developpement"
+      ]
+    },
+    "Path": "./developpement/web",
+    "Slug": "web",
+    "Title": "web/"
+  },
+  {
+    "ID": "wiki",
+    "Meta": {
+      "tags": [
+        "root/configuration"
+      ]
+    },
+    "Path": "./configuration/wiki",
+    "Slug": "wiki",
+    "Title": "wiki/"
+  }
+]

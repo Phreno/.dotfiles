647d8430ddb763c7a8a9825e5c50103701f6fda1
Etienne Audouin <etienne.audouin-ext@pole-emploi.fr>
2022-10-08 19:06:10 +0200

    test(builder): Ajoute un boyBuilder

---
+++ code-template/java/patterns/builder/app/src/test/java/builder/BoyBuilder.java
@@ -0,0 +1,26 @@
+package builder;
+
+public class BoyBuilder {
+
+    public void buildCompany() {
+    }
+
+    public void buildPicture() {
+    }
+
+    public void buildAge() {
+    }
+
+    public void buildName() {
+    }
+
+    public void buildGender() {
+    }
+
+    public void buildEmail() {
+    }
+
+    public void buildBalance() {
+    }
+
+}

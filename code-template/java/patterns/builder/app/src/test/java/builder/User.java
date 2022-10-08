61ea10dcea5c08323d574ceae7e71210726c43f6
Etienne Audouin <etienne.audouin-ext@pole-emploi.fr>
2022-10-08 18:53:36 +0200

    test(builder): Ajoute un objet utilisateur

---
+++ code-template/java/patterns/builder/app/src/test/java/builder/User.java
@@ -0,0 +1,69 @@
+package builder;
+
+public class User {
+
+    private String age;
+    private String balance;
+    private String company;
+    private String email;
+    private String gender;
+    private String name;
+    private String picture;
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+}
